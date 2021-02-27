af9202357e0cd2b4926bf3eb8bdf536a20d3b4ce
Engin Karataş <enginkaratas99@gmail.com>
2021-02-27 20:16:58 +0300

    Update Solution.java

--- javacase/Solution.java
+++ javacase/Solution.java
@@ -5,30 +5,53 @@ package javacase;
  */
 public class Solution {
     public static void main(String[] args) {
-        int data[] = { 3, 2, 3, 4, 1, 2, 3, 3, 3, 3, 3 };
+        int data[] = { 8, 1, 3, 3, 1, 2, 7, 2, 1, 7, 9, 7 };
         int n = 5;
         solution(data, n);
     }
 
     private static void solution(int data[], int n) {
-        // if data[i]
-        boolean collideOnce = false;
+        int dataLength = data.length;
+        int maxnumber = 0;
 
-        int countArray[] = new int[data.length];
-        for (int i = 0; i < data.length; i++) {
-            for (int j = i + 1; j < data.length; j++) {
-                if (data[i] == data[j]) {
-                    System.out.println("i  : " + i + "    j  : " + j);
-                    System.out.println(data[i]);
-                    countArray[data[j]] += 1;
-
-                }
+        int i = 0;
+        while (i < dataLength) {
+            if (data[i] <= 0) {
+                i++;
+                continue;
             }
-        }
+            int elementIndex = data[i] - 1;
+            if (data[elementIndex] > 0) {
+                data[i] = data[elementIndex];
+                data[elementIndex] = -1;
+            } else {
+                data[elementIndex]--;
+                data[i] = 0;
+                i++;
+            }
 
-        for (int i = 0; i < countArray.length; i++) {
-            System.out.println("countArray[" + i + "]" + "--> value :  " + countArray[i]);
-        }
+            System.out.println("Below are counts of all elements");
+            for (int j = 0; j < n; j++)
+                System.out.println(j + 1 + "->" + Math.abs(data[j]));
 
+            // //detect what is maxnumber for data array, for new heap size countArray
+            // for (int i = 0; i < data.length; i++) {
+            // maxnumber = data[i];
+            // if (data[i] > maxnumber) {
+            // data[i] = maxnumber;
+            // }
+            // }
+
+            // int[] countArray = new int[maxnumber+1];
+
+            // //countArray dizisinin [i] inci elemanı n`den büyük eşitse, sil -
+            // for (int i = 0; i < dataLength; i++) {
+            // if (countArray[i] >= n) {
+            // System.out.println(cou);
+            // }
+
+            // }
+
+        }
     }
 }
